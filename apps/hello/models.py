c2b33b1bec3403dd55073510351895143c07e714
alexaled <alexaled@gmail.com>
2015-10-06 20:55:05 +0300

    ticket1:model

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 
+class MyBio(models.Model):
+    first_name = models.CharField(u"Name", max_length=50)
+    last_name = models.CharField(u"Last name", max_length=50)
+    birth_date = models.DateField(u"Date of birth", blank=True, null=True)
+    biography = models.TextField(u"Bio", blank=True, null=True)
+    email = models.EmailField(u"email", blank=True, null=True)
+    skype = models.CharField(u"Skype", max_length=200, blank=True, null=True)
+    jabber = models.EmailField(u"Jabber", blank=True, null=True)
+    other_contacts = models.TextField(u"Other contacts", blank=True, null=True)
+
+    def __str__(self):
+        return "Bio data for {0} {1}".format(self.first_name, self.last_name)
--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -6,8 +6,12 @@ from hello.models import MyBio
 class BioModelTests(TestCase):
 
     def create_my_bio_test_data(self):
-        return MyBio.objects.create(first_name='John',
-                                    last_name='Smith',
+        """
+        Method for creating test data
+        :return:
+        """
+        return MyBio.objects.create(first_name='Oleksii',
+                                    last_name='Aledinov',
                                     birth_date=datetime.date.today(),
                                     biography='Info about me',
                                     email='alexaled@gmail.com',
@@ -16,22 +20,38 @@ class BioModelTests(TestCase):
                                     other_contacts='0968962750')
 
     def test_bio_model_str(self):
+        """
+        check object string representation
+        :return:
+        """
         bio = MyBio(first_name='Oleksii', last_name='Aledinov')
         self.assertEquals(str(bio), 'Bio data for Oleksii Aledinov')
 
     def test_create_bio(self):
+        """
+        checked model to creating object
+        :return:
+        """
         self.assertEquals(MyBio.objects.count(), 0)
         self.create_my_bio_test_data()
         self.assertEquals(MyBio.objects.count(), 1)
 
     def test_update_doctor(self):
+        """
+        checked model to updating object
+        :return:
+        """
         bio = self.create_my_bio_test_data()
         bio.first_name = 'JJ'
         bio.save()
-        bio_test = bio.objects.get(pk=1)
+        bio_test = MyBio.objects.get(pk=1)
         self.assertEquals(str(bio_test), 'Bio data for JJ Aledinov')
 
     def test_delete(self):
+        """
+        checked model to deleting object
+        :return:
+        """
         self.assertEquals(MyBio.objects.count(), 0)
         self.create_my_bio_test_data()
         self.assertEquals(MyBio.objects.count(), 1)
