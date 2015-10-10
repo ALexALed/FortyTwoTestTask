c1c02a11a166444dae3072965dd41cd8c6f15304
alexaled <alexaled@gmail.com>
2015-10-10 18:55:13 +0300

    ticket:1 tests done

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -9,7 +9,7 @@ class MyBioView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MyBioView, self).get_context_data(**kwargs)
         bio_data_count = MyBio.objects.count()
-        if bio_data_count == 1:
+        if bio_data_count >= 1:
             context['object'] = MyBio.objects.all()[0]
         else:
             raise Http404
